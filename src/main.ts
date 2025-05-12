d1d8f0a69d96c0f95886748a510a13b8fc6a69d4
Daniil Khmelev <Danila_Foxek@mail.ru>
2025-05-12 17:24:48 +0600

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Trello-like api')
+    .setDescription('Trello-like api description')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactort = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactort);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
